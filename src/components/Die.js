22d1cfbddc2324865547c0a220109d76f49680a0
Harrison Pan <hanshen36@gmail.com>
2022-04-15 02:36:06 +1200

    Add css to put real dots on the dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,15 +1,46 @@
+import { nanoid } from 'nanoid'
+
 export default function Die(props) {
   // use inline class
-  // const styles = {
-  //   backgroundColor: props.isHeld ? '#59E391' : 'white',
-  // }
-  // <div className="die-face" style={styles}>
+  const styles = {
+    backgroundColor: props.isHeld ? '#59E391' : '#e7e7e7',
+  }
+
+  const dotElement = <span className="dot"></span>
+  const oneDotElement = <div className="column">{[dotElement]}</div>
+  const twoDotsElement = (
+    <div className="column">{[dotElement, dotElement]}</div>
+  )
+  const threeDotsElement = (
+    <div className="column">{[dotElement, dotElement, dotElement]}</div>
+  )
+
+  const getDiceElements = () => {
+    switch (props.value) {
+      case 1:
+        return dotElement
+      case 2:
+        return [dotElement, dotElement]
+      case 3:
+        return [dotElement, dotElement, dotElement]
+      case 4:
+        return [twoDotsElement, twoDotsElement]
+      case 5:
+        return [twoDotsElement, oneDotElement, twoDotsElement]
+      case 6:
+        return [threeDotsElement, threeDotsElement]
+      default:
+        break
+    }
+  }
+
   return (
     <div
-      className={props.isHeld ? 'die-face held' : 'die-face'}
+      className={`dice face-${props.value}`}
+      style={styles}
       onClick={props.toggleHold}
     >
-      <h2 className="die-num">{props.value}</h2>
+      {getDiceElements()}
     </div>
   )
 }
